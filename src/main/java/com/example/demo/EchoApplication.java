38f7e343d9115e8695fa7431c54e0a43111d5702
Wala86 <benamorwala@yahoo.fr>
2017-08-17 16:23:28 +0200

    Update EchoApplication.java

--- src/main/java/com/example/demo/EchoApplication.java
+++ src/main/java/com/example/demo/EchoApplication.java
@@ -12,6 +12,11 @@ import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
+import ai.api.AIConfiguration;
+import ai.api.AIDataService;
+import ai.api.model.AIRequest;
+import ai.api.model.AIResponse;
+
 @SpringBootApplication
 @LineMessageHandler
 public class EchoApplication {
@@ -21,8 +26,40 @@ public class EchoApplication {
 
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-        System.out.println("event: " + event);
-        return new TextMessage(event.getMessage().getText()+"rihab");
+       		System.out.println("event: " + event);
+		/*
+		 * if (event.getMessage().getText() == "Hi" ||
+		 * event.getMessage().getText().equals("Hi")) { return new
+		 * TextMessage("Hello, How are you?"); } else { return new
+		 * TextMessage(event.getMessage().getText()); }
+		 */
+		AIConfiguration configuration = new AIConfiguration("f01696a0854347a1996bd9bc6155dacb");
+
+		AIDataService dataService = new AIDataService(configuration);
+
+		try {
+			AIRequest request = new AIRequest(event.getMessage().getText());
+
+			AIResponse response = dataService.request(request);
+
+			if (response.getStatus().getCode() == 200) {
+				System.out.println(response.getResult().getFulfillment().getDisplayText());
+			} else {
+				System.err.println(response.getStatus().getErrorDetails());
+			}
+			
+			
+			if (response.getResult().getAction() == "Country") {
+				System.out.println(event.getMessage().getText() + ":Country");
+				return new TextMessage(event.getMessage().getText() + ":Country");
+			} else {
+				System.err.println(event.getMessage().getText() + ":NOT-Country");
+				return new TextMessage(event.getMessage().getText() + ":NOT-Country");
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
     }
 
     @EventMapping
